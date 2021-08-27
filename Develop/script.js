311a8aed4afde454bec24809ae03ad5b13176a7e
Sheri Foster <87589967+SFosR@users.noreply.github.com>
2021-08-27 13:12:06 -0400

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,29 @@
+//GIVEN I am using a daily planner to create a schedule
+//WHEN I open the planner(html) THEN the current day is displayed at the top of the calendar
+//  1) need a running date
+//  2) I need to be able to scroll down with timeblocks for standard business hours 
+//  3) need multiple containers. One for every:
+//          A)hour
+//          B)space for event/timeblock for that day
+//            b1) must be color coded for past, present or future  
+//            b2) clickable so event can be entered
+//            b3) save button to local storage  
+//          C)blue tab
+//  4) WHEN I refresh the page, THEN the saved events persist(local storage?)
+
+
+
+
+//moment().calendar {
+    //sameDay: ["dddd, MMMM Do YYYY, h:mm");
+
+
+// 
+
+
+
+
+
+
+
+
