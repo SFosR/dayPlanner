917f5741960df5b611f22915b7fc5765969b7a6b
Sheri Foster <87589967+SFosR@users.noreply.github.com>
2021-08-28 23:37:35 -0400

    Additional changes to date and more

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,18 @@
-//GIVEN I am using a daily planner to create a schedule
-//WHEN I open the planner(html) THEN the current day is displayed at the top of the calendar
-//  1) need a running date
-//  2) I need to be able to scroll down with timeblocks for standard business hours 
-//  3) need multiple containers. One for every:
-//          A)hour
-//          B)space for event/timeblock for that day
-//            b1) must be color coded for past, present or future  
-//            b2) clickable so event can be entered
+
 //            b3) save button to local storage  
 //          C)blue tab
 //  4) WHEN I refresh the page, THEN the saved events persist(local storage?)
 
 
+var currentDay = moment().format("dddd, MMMM Do, YYYY")
+$("#currentDay").text(currentDay);
 
 
-//moment().calendar {
-    //sameDay: ["dddd, MMMM Do YYYY, h:mm");
+const buttonInput = document.querySelector('.timeblock');
+const text = document.querySelector('.text');
+const button = document.querySelector('.savBtn');
 
+//buttonInput.addEventListener('click', function() { 
+// What am I doing wrong here? 
 
-// 
-
-
-
-
-
-
-
-
+//localStorage.getItem('textinput', text.textContent)
